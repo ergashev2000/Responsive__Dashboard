2a8ecb0c9f339c8d6622c483082f04b523192a40
islomjon <ergashevislomjon1020@gmail.com>
2022-07-20 13:23:29 +0500

    New

---
+++ js/orders.js
@@ -0,0 +1,40 @@
+const Orders = [
+    {
+        productName: 'Ergashev Islomjon',
+        productNumber: '789943',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Ergashev Ilyosjon',
+        productNumber: '46546',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ergashev Iqboljon',
+        productNumber: '324132',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Salimov Azizbek',
+        productNumber: '392743',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Hoshimov Anvar',
+        productNumber: '21434',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lazizov Rustam',
+        productNumber: '344322',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    }
+    
+
+]
